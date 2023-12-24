5f586260951dac0dab3867fac295264c2185cf9d
shahariaz <shahariaz.info@gmail.com>
2023-12-24 23:08:39 +0600

    product list

---
+++ frontend/src/types/Product.ts
@@ -0,0 +1,12 @@
+export type Product = {
+  name: string;
+  slug: string;
+  image: string;
+  category: string;
+  price: number;
+  brand: string;
+  rating: number;
+  description: string;
+  numReviews: number;
+  countInStock: number;
+};
